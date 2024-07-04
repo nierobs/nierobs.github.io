cacdbac7bb133c2c8d4c62ad5ff480488557f8a6
Robson Niemeyer <nierobs@hotmail.com>
2024-07-04 20:41:11 -0300

    Update style

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
 'use strict'
+
+const nodes = document.querySelectorAll('section');
+
+for (let i = 0; i < nodes.length; i++) {
+    if (window.innerHeight - nodes[i].offsetTop <= 0) {
+        nodes[i].style.opacity = 0;
+    }
+}
+
+window.addEventListener('scroll', (event) => {
+    for (let i = 0; i < nodes.length; i++) {
+        if (window.innerHeight + window.scrollY - nodes[i].offsetTop > 0) {
+            nodes[i].classList.add('animation--fadein');
+        }
+    }
+});
