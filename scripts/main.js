27771afedf5a49e0805dbdeb68ed8e0f2d7e8bc5
Robson Niemeyer <nierobs@hotmail.com>
2024-07-21 01:25:16 -0300

    Update script

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,25 @@
 'use strict'
 
 /* sass animation fade-in component
-*/
+ */
 const nodes = document.getElementsByClassName('animation--fade-in');
 
-for (let i = 0; i < nodes.length; i++) {
-    if (window.innerHeight - nodes[i].offsetTop <= 0) {
-        nodes[i].style.opacity = 0;
-    }
-}
-
-window.addEventListener('scroll', () => {
+window.addEventListener('load', () => {
     for (let i = 0; i < nodes.length; i++) {
-        if (window.innerHeight + window.scrollY - nodes[i].offsetTop > 0) {
-            nodes[i].classList.add('animation--fade-in--show');
+        if (window.scrollY + window.innerHeight - nodes[i].offsetTop <= 0) {
+            nodes[i].style.opacity = 0;
         }
     }
+
+    window.addEventListener('scroll', () => {
+        for (let i = 0; i < nodes.length; i++) {
+            if (window.scrollY + window.innerHeight - nodes[i].offsetTop > 0) {
+                nodes[i].classList.add('animation--fade-in--show');
+            }
+        }
+    });
 });
 
 /* biati-digital/glightbox
-*/
+ */
 const lightbox = GLightbox({ selector: '.glightbox' });
